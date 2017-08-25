71fd5d5b8b9243618dd153c497a075de83c312a9
aczender <czender.a@gmail.com>
2017-08-25 15:44:36 +0200

    2017.08.25.

--- app/src/main/java/com/example/andrew/habittracker/MainActivity.java
+++ app/src/main/java/com/example/andrew/habittracker/MainActivity.java
@@ -1,125 +1,153 @@
 package com.example.andrew.habittracker;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.example.andrew.habittracker.data.HabitContract;
+import com.example.andrew.habittracker.data.HabitContract.TaskEntry;
 import com.example.andrew.habittracker.data.TaskDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mTaskEditText;
-    private EditText mHourEditText;
-    private Spinner mDateSpinner;
-
-    private int mDate = HabitContract.TaskEntry.DATE_PICK;
+    private TaskDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catalog);
 
-        // Find all relevant views that we will need to read user input from
-        mTaskEditText = (EditText) findViewById(R.id.edit_task_name);
-        mHourEditText = (EditText) findViewById(R.id.edit_hour);
-        mDateSpinner = (Spinner) findViewById(R.id.spinner_date);
-
-        setupSpinner();
-    }
-
-    private void setupSpinner() {
-        ArrayAdapter dateSpinnerAddapter = ArrayAdapter.createFromResource(this, R.array
-                .array_date_options, android.R.layout.simple_spinner_item);
-
-        dateSpinnerAddapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        mDateSpinner.setAdapter(dateSpinnerAddapter);
-
-        mDateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selection = (String) parent.getItemAtPosition(position);
-                if (!TextUtils.isEmpty(selection)) {
-                    if (selection.equals("Monday")) {
-                        mDate = HabitContract.TaskEntry.DATE_MONDAY;
-                    } else if (selection.equals("Tuesday")) {
-                        mDate = HabitContract.TaskEntry.DATE_TUESDAY;
-                    } else if (selection.equals("Wednesday")) {
-                        mDate = HabitContract.TaskEntry.DATE_WEDNESDAY;
-                    } else if (selection.equals("Thursday")) {
-                        mDate = HabitContract.TaskEntry.DATE_THURSDAY;
-                    } else if (selection.equals("Friday")) {
-                        mDate = HabitContract.TaskEntry.DATE_FRIDAY;
-                    } else if (selection.equals("Saturday")) {
-                        mDate = HabitContract.TaskEntry.DATE_SATURDAY;
-                    } else if (selection.equals("Sunday")) {
-                        mDate = HabitContract.TaskEntry.DATE_SUNDAY;
-                    } else {
-                        mDate = HabitContract.TaskEntry.DATE_PICK;
-                    }
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                mDate = HabitContract.TaskEntry.DATE_PICK;
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
             }
         });
+
+        mDbHelper = new TaskDbHelper(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayDatabaseInfo();
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the pets database.
+     */
+    private void displayDatabaseInfo() {
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Perform this raw SQL query "SELECT * FROM pets"
+        // to get a Cursor that contains all rows from the pets table.
+        String[] projection = {
+                TaskEntry._ID,
+                TaskEntry.COLUMN_TASK_NAME,
+                TaskEntry.COLUMN_DATE,
+                TaskEntry.COLUMN_HOUR
+        };
+
+        Cursor cursor = db.query(
+                TaskEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+        try {
+            // Create a header in the Text View that looks like this:
+            //
+            // The pets table contains <number of rows in Cursor> pets.
+            // _id - name - breed - gender - weight
+            //
+            // In the while loop below, iterate through the rows of the cursor and display
+            // the information from each column in this order.
+
+            displayView.setText("The chores table contains " + cursor.getCount() + " chores.\n\n");
+            displayView.append(TaskEntry._ID + " - " +
+                    TaskEntry.COLUMN_TASK_NAME + " - " +
+                    TaskEntry.COLUMN_DATE + " - " +
+                    TaskEntry.COLUMN_HOUR + "\n");
+
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(TaskEntry._ID);
+            int taskColumnIndex = cursor.getColumnIndex(TaskEntry.COLUMN_TASK_NAME);
+            int dateColumnIndex = cursor.getColumnIndex(TaskEntry.COLUMN_DATE);
+            int hourColumnIndex = cursor.getColumnIndex(TaskEntry.COLUMN_HOUR);
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentTask = cursor.getString(taskColumnIndex);
+                String currentDate = cursor.getString(dateColumnIndex);
+                int currentHour = cursor.getInt(hourColumnIndex);
+                // Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(("\n" + currentID + " - " +
+                        currentTask + " - " +
+                        currentDate + " - " +
+                        currentHour));
+            }
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
     }
 
     private void insertTask() {
-        String taskString = mTaskEditText.getText().toString().trim();
-        String hourString = mHourEditText.getText().toString().trim();
-        int time = Integer.parseInt(hourString);
-
-        TaskDbHelper mDbHelper = new TaskDbHelper(this);
-
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
         ContentValues values = new ContentValues();
 
-        values.put(HabitContract.TaskEntry.COLUMN_TASK_NAME, taskString);
-        values.put(HabitContract.TaskEntry.COLUMN_DATE, mDate);
-        values.put(HabitContract.TaskEntry.COLUMN_HOUR, time);
+        values.put(TaskEntry.COLUMN_TASK_NAME, "Workout");
+        values.put(TaskEntry.COLUMN_DATE, TaskEntry.DATE_MONDAY);
+        values.put(TaskEntry.COLUMN_HOUR, 2);
 
-        long newRowId = db.insert(HabitContract.TaskEntry.TABLE_NAME, null, values);
+        long newRowId = db.insert(TaskEntry.TABLE_NAME, null, values);
 
-        if (newRowId == -1) {
-            Toast.makeText(this, "Error with adding new task", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Task saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertTask();
+                displayDatabaseInfo();
+
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                // Do nothing for now
+                return true;
         }
-
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu){
-            getMenuInflater().inflate(R.menu.menu_editor, menu);
-            return true;
-        }
-
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            switch (item.getItemId()) {
-                case R.id.action_save:
-                    insertTask();
-                    finish();
-                    return true;
-                case R.action_delete:
-                    return true;
-                case android.R.id.home:
-                    NavUtils.navigateUpFromSameTask(this);
-                    return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
+        return super.onOptionsItemSelected(item);
     }
 }
